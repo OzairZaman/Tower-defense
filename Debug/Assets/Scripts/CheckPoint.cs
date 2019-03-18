a96e7b89ea3d11396145edb189223b57ae6943fe
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-03-18 14:38:55 +1100

    Debug Class Test

---
+++ Debug/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script can be found in the Component section under the option Intro RPG/Player/Check Point
+public class CheckPoint : MonoBehaviour 
+{
+	#region Variables
+	//[Header("Check Point Elements")]
+	//transform for our currentCheck
+	//[Header("Character Health")]
+	//character Health script that holds the players health
+	#endregion
+	#region Start
+		//Reference to the character health script component attached to our player
+		#region Check if we have Key
+		//if we have a save key called SpawnPoint
+			//then our checkpoint is equal to the game object that is named in out save file or the float x,y,z
+			//our transform.position is equal to that of the checkpoint or to the float x,y,z
+		#endregion
+	#endregion
+	#region Update
+		//if our characters health is less than or equal to 0
+			//our transform.position is equal to that of the checkpoint or float x,y,z
+			//our characters health is equal to full health
+			//character is alive
+			//characters controller is active		
+	#endregion
+	#region OnTriggerEnter
+	//Collider other
+		//if our other objects tag when compared is CheckPoint
+			//our checkpoint is equal to the other objects transform
+			//save our SpawnPoint as the name of the check point or float x,y,z
+	#endregion
+}
+
+
+
+
+
---
+++ Debug/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Intro PRG/RPG/Player/Movement")]
+public class Movement : MonoBehaviour 
+{
+    #region Variables
+    [Header("PLAYER MOVEMENT")]
+    [Space(10)]
+    [Header("Characters Move Direction")]
+    public Vector3 moveDirection;
+    private CharacterController _charC;
+    [Header("Character Variables")]
+    public float jumpSpeed = 8.0f;
+    public float speed = 5;
+    public float gravity = 20;
+    //[Header("")]
+    #endregion
+    #region Start
+    private void Start()
+    {
+        _charC = GetComponent<CharacterController>();
+    }
+    #endregion
+    #region Update
+    private void Update()
+    {
+        if(_charC.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+            moveDirection = transform.TransformDirection(moveDirection);
+            
+
+            if(Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        
+        moveDirection.y -= gravity * Time.deltaTime;
+        _charC.Move(moveDirection * Time.deltaTime);
+    }
+    #endregion
+
+}
+//16 errors
