52736578fb259c9ecda984467afb1ff00b9746d4
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-03-26 17:18:13 +1100

    Camera stuff

---
+++ TowerDefense/Assets/Scripts/Cameras/CameraOrbit.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+
+    public Camera attachedCamera; // this is for drag and drop in unity
+    [Header("Orbit")]
+    public float xSpeed = 120f, ySpeed = 120f;
+    public float yMinLimit = -20f, yMaxLimit = 80f;
+    [Header("Collision")]
+    public bool cameraCollision = true; // is Camera Collision enabled?
+    public bool ignoreTriggers = true; // will the sphere cast ignore triggers?
+    public float castRadius = .3f; //Radius of shpere cast
+    public float castDistance = 1000f; // Distance the cast travels
+    public LayerMask hitLayers; // Layers that casting will hit
+
+    private float originalDistance; // Record starting distance of camera
+    private float distance; // Current Distance of camera
+    private float x, y; // X and Y Mouse Rotation
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attachedCamera.transform.position, castRadius);
+    }
+
+
+    void Start ()
+    {
+        // Set original distance
+        originalDistance = Vector3.Distance(transform.position, attachedCamera.transform.position);
+        // Set X and Y degrees to current camera rotation
+        x = transform.eulerAngles.y;
+        y = transform.eulerAngles.x;
+    }
+	
+	
+	void Update ()
+    {
+        // is right mouse button presed
+        if (Input.GetMouseButton(1))
+        {
+            //disable cursor
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            // orbit
+            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+            y -= Input.GetAxis("Mouse Y") * xSpeed * Time.deltaTime;
+
+            y = Mathf.Clamp(y, yMinLimit, yMaxLimit);
+
+            // Rotate the transform using Euler angles
+            this.transform.rotation = Quaternion.Euler(y, x, 0);
+        }
+        else
+        {
+            //enable cursor
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        /// Set distance to original distance
+        distance = originalDistance;
+
+        // Change distance to what we hit
+        // Is camera collision enabled?
+        if (cameraCollision)
+        {
+            // Create a ray starting from orbit position and going in the direction of the camera
+            Ray camRay = new Ray(transform.position, -transform.forward);
+            RaycastHit hit; // Stores the hit information after cast
+            // Shoot a sphere behind the camera
+            if (Physics.SphereCast(camRay, // Ray in the directon of camera
+                                   castRadius, // How thicc the sphere is
+                                   out hit, // The hit information collected
+                                   castDistance, // How far the cast goes
+                                   hitLayers, // What layers we're allowed to hit
+                                   ignoreTriggers ? // Ignore triggers?
+                                    QueryTriggerInteraction.Ignore // Ignore it!
+                                   : // Else
+                                    QueryTriggerInteraction.Collide)) // Don't ignore
+            {
+                //set distance to distance of hit
+                distance = hit.distance;
+            }
+        }
+        
+        // Apply distance to cameras
+        attachedCamera.transform.position = transform.position - transform.forward * distance;
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Cameras/CameraPan.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+    public Camera attachedCamera;
+    public float movementThreshold = .25f; // Percentage offset where move starts (25%)
+    public float movementSpeed = 20f;
+    public float zoomSensitivity = 10;
+    public Vector3 size = new Vector3(20f, 1f, 20f);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    Vector3 GetAdjustedPos(Vector3 incomingPos)
+    {
+        Vector3 pos = transform.position;
+        Vector3 halfSize = size * .5f;
+
+
+
+        // X
+        if (incomingPos.x > pos.x + halfSize.x) incomingPos.x = pos.x + halfSize.x;
+        if (incomingPos.x < pos.x - halfSize.x) incomingPos.x = pos.x - halfSize.x;
+
+        // Y
+        if (incomingPos.y > pos.y + halfSize.y) incomingPos.y = pos.y + halfSize.y;
+        if (incomingPos.y < pos.y - halfSize.y) incomingPos.y = pos.y - halfSize.y;
+
+        // Z
+        if (incomingPos.z > pos.z + halfSize.z) incomingPos.z = pos.z + halfSize.z;
+        if (incomingPos.z < pos.z - halfSize.z) incomingPos.z = pos.z - halfSize.z;
+
+
+        return incomingPos;
+    }
+
+    void Movement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Create a transform with a smaller name
+            Transform camTransform = attachedCamera.transform;
+
+            //get mouse to viewport coordinates
+            Vector2 mousePOint = attachedCamera.ScreenToViewportPoint(Input.mousePosition);
+            Vector2 offest = mousePOint - new Vector2(.5f, .5f);
+
+            Vector3 input = Vector3.zero;
+            if (offest.magnitude > movementThreshold)
+            {
+                input = new Vector3(offest.x, 0, offest.y) * movementSpeed;
+            }
+
+            // get scroll from axis and multiply by zoomSensitivity
+            float inputScroll = Input.GetAxis("Mouse ScrollWheel");
+            Vector3 scroll = camTransform.forward * inputScroll * zoomSensitivity;
+
+            /// Apply movement
+            Vector3 movement = input + scroll;
+            camTransform.position += movement * Time.deltaTime;
+
+            // Filter
+            camTransform.position = GetAdjustedPos(camTransform.position);
+        }
+        
+        
+    }
+
+
+
+
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+}
