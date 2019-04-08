3be50ee8c2f66fb294d878acd4a75d971e3629d4
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-04-08 17:13:38 +1000

    debug

--- TowerDefense2D/Assets/Scripts/Money.cs
+++ TowerDefense2D/Assets/Scripts/Money.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Money : MonoBehaviour {
 
     public float money;
+
+    //void Start()
+    //{
+    //    DontDestroyOnLoad(this.gameObject);
+    //    SceneManager.LoadScene(1);
+    //}
 }
